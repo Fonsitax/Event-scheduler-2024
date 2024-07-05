33708fda86db91a1788f0eaf669c25c59bbda753
Christoph Kuri <chkuri@chkuri.de>
2024-07-05 08:55:26 +0200

    Update Sign-in token to localStorage

--- src/components/fetchdata.jsx
+++ src/components/fetchdata.jsx
@@ -1,19 +1,28 @@
 import React, { useState, useEffect } from 'react';
 
+//const [users, setUsers] = useState();
+
 // User anlegen
 
 export const addNewUser = async (mail, passw) => {
-  /*  console.log(typeof mail);
-  console.log(typeof passw); */
   try {
     const response = await fetch('http://localhost:3001/api/users', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email: mail, password: passw }),
     });
+
     const data = await response.json();
+
     console.log(data);
-    return data;
+
+    if (data.error) {
+      window.alert(data.error);
+    } else {
+      window.alert(`Willkommen an Board : ${mail}  !`);
+    }
+
+    return;
   } catch (error) {
     console.error('Fehler beim Abrufen der Daten von API1:', error);
   }
@@ -21,7 +30,36 @@ export const addNewUser = async (mail, passw) => {
 
 // User login - Abfrage ob User existier -
 
-export const userlogin = async () => {};
+export const userlogin = async (mail, passw) => {
+  try {
+    const response = await fetch('http://localhost:3001/api/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email: mail, password: passw }),
+    });
+
+    const data = await response.json();
+
+    let token = data.token;
+
+    localStorage.clear();
+
+    localStorage.setItem(mail, token);
+    /*     console.log(response);
+    console.log(data.token);
+    console.log(typeof data); */
+
+    if (data.error) {
+      window.alert(data.error);
+    } else {
+      window.alert(`Schön das du wieder hier bist : ${mail}  !`);
+    }
+
+    return;
+  } catch (error) {
+    console.error('Fehler beim Abrufen der Daten von API1:', error);
+  }
+};
 
 // Event abrufen
 
--- src/components/sign-in.jsx
+++ src/components/sign-in.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { userlogin } from './fetchdata';
 
 export default function SignIn() {
+  const [password, setpassword] = useState('');
+  const [mail, setmail] = useState('');
+
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -12,7 +16,13 @@ export default function SignIn() {
           </p>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form className="card-body">
+          <form
+            className="card-body"
+            onSubmit={(e) => {
+              e.preventDefault();
+              userlogin(mail, password);
+            }}
+          >
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -22,6 +32,8 @@ export default function SignIn() {
                 placeholder="email"
                 className="input input-bordered"
                 required
+                value={mail}
+                onChange={(e) => setmail(e.target.value)}
               />
             </div>
             <div className="form-control">
@@ -33,6 +45,8 @@ export default function SignIn() {
                 placeholder="password"
                 className="input input-bordered"
                 required
+                value={password}
+                onChange={(e) => setpassword(e.target.value)}
               />
               <label className="label">
                 <a
