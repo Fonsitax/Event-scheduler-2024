16324c321d4bcaa00430df1437fa7a597c8e409e
Henry Nguyen <henrylinh94@yahoo.de>
2024-07-05 13:03:07 +0200

    display events on homepage

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,37 +1,55 @@
 import { Link } from 'react-router-dom';
-
-// Beispiel Events
-const events = [
-  {
-    id: 1,
-    title: 'Event 1',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus tempora iste dolor, molestiae unde recusandae ipsam sit consequatur consectetur. Porro corrupti hic soluta illo accusamus cupiditate consequuntur, animi quo distinctio.',
-    date: '2023-04-01',
-  },
-  {
-    id: 2,
-    title: 'Event 2',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores cumque veniam sed sint, est voluptatum. Quisquam odio cupiditate excepturi ducimus ab, magnam aspernatur earum minima voluptatem, iusto error deleniti non!',
-    date: '2023-04-15',
-  },
-];
+import { useState, useEffect } from 'react';
 
 export default function HomePage() {
+
+  const [events, setEvents] = useState([]);
+
+  const url = 'http://localhost:3001/api/events';
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await fetch(url, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        console.log(response);
+        if (!response.ok) {
+          throw new Error('Fehler beim Laden der Events');
+        }
+        const data = await response.json();
+        console.log(data);
+        if (Array.isArray(data.results)) {
+        setEvents(data.results);
+      } else {
+        console.error('Array mit Events erwartet')
+        setEvents([]);
+      }
+    } catch (error) {
+        console.error('Fehler beim Laden der Events:', error);
+      }
+    };
+
+    fetchEvents();
+
+  }, []);
+
+  console.log(events);
+
   return (
-    <div className="container mx-auto p-14">
+    <div className="container mx-auto mt-10 p-14">
       <h1 className="text-3xl font-bold mb-4">Alle Events:</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {events.map((event) => (
-          <Link
-            to={`/event-details/${event.id}`}
-            key={event.id}
-            className="card card-bordered shadow-lg hover:shadow-xl transition-shadow duration-300"
-          >
+          <div key={event.id} className="card card-bordered shadow-lg hover:shadow-xl transition-shadow duration-300">
+            <Link
+            to={`/event-details/${event.id}`}>
             <div className="card-body p-4 border-2 rounded-md">
               <img
-                src="https://picsum.photos/seed/picsum/500/200"
+                src="https://picsum.photos/500/200"
                 alt="Event Image"
                 className="mb-4"
               />
@@ -41,6 +59,7 @@ export default function HomePage() {
               <p>{event.description}</p>
             </div>
           </Link>
+          </div>
         ))}
       </div>
     </div>
